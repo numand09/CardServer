51b20efd22eaf8633176e64da6be1a513d999544
Umit Numan Duman <129855966+numand09@users.noreply.github.com>
2025-11-30 03:02:00 +0300

    Revert "SRP"
    This reverts commit bbd8af7fc79971dda520d5014af7c3c1d6673a64.

--- routes/matchmakingRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { findMatch, leaveMatch } = require('../controllers/matchmakingController');
-
-router.post('/find-match', findMatch);
-router.post('/leave-match', leaveMatch);
-
-module.exports = router;
